d29ec30a58522674f23a589ef1a960571ecb2c48
Vaishnavi Kesherwani <122589971+Vaishu7605@users.noreply.github.com>
2024-05-27 12:15:27 +0530

    Add files via upload

---
+++ classes1.java
@@ -0,0 +1,24 @@
+
+class math {
+
+    public int addi(int a, int b) {
+        return a + b;
+    }
+}
+
+public class Classes1 {
+
+    public static void main(String[] args) {
+        math a1 = new math();
+        int x = 4;
+        int y = 6;
+        int z = a1.addi(x, y);
+        System.out.println(z);
+
+        int x1 = 3;
+        int y1 = 5;
+        int z1 = a1.addi(x1, y1);
+        System.out.println(z1);
+
+    }
+}
